89d57d1326f5f405e2b0604b0bb79b918e906293
kwonsean <gownn12@naver.com>
2021-12-21 00:57:43 +0900

    feat: 찜목록 페이지 기본 기능 구현 완료

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'reactstrap'
+import ProductList from './ProductList'
+import ProductSearchBar from './ProductSearchBar'
 
-const Product = (props) => {
+const Product = ({ userId }) => {
+  const [zzimList, setZzimList] = useState([])
+
+  const selectedProductList = (
+    category1 = '',
+    category2 = '',
+    category3 = '',
+    category4 = ''
+  ) => {
+    axios
+      .post('api/product?type=list', {
+        user_id: userId,
+        category1,
+        category2,
+        category3,
+        category4,
+        title: '',
+      })
+      .then((response) => {
+        let data = response.data.json
+        setZzimList(data)
+      })
+      .catch((error) => console.log(error))
+  }
+  useEffect(() => {
+    selectedProductList()
+  }, [])
   return (
-    <div>
+    <Container>
       <h2>찜 목록</h2>
-    </div>
+      <ProductSearchBar selectedProductList={selectedProductList} />
+      <ProductList
+        zzimList={zzimList}
+        selectedProductList={selectedProductList}
+      />
+    </Container>
   )
 }
 
