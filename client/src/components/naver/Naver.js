345c66d179d29744ba011502a9fb8cad3dbec458
kwonsean <gownn12@naver.com>
2021-12-20 20:20:28 +0900

    feat: 상품 검색 페이지 전체 연결

--- client/src/components/naver/Naver.js
+++ client/src/components/naver/Naver.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'reactstrap'
+import SearchBar from './SearchBar'
+import ShoppingList from './ShoppingList'
 
 const Naver = (props) => {
+  const [searchList, setSearchList] = useState([])
+  const [selectedText, setSelectedText] = useState('')
+  const [totalResults, setTotalResults] = useState(0)
   return (
-    <div>
+    <Container>
       <h2>상품 검색 & 찜하기</h2>
-    </div>
+      <SearchBar
+        setSearchList={setSearchList}
+        searchList={searchList}
+        selectedText={selectedText}
+        setSelectedText={setSelectedText}
+        setTotalResults={setTotalResults}
+      />
+      <ShoppingList
+        setSearchList={setSearchList}
+        searchList={searchList}
+        selectedText={selectedText}
+        totalResults={totalResults}
+      />
+    </Container>
   )
 }
 
