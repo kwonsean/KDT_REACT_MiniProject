22abb0b3644fd202551a88b5b998b69cadf1b6fd
kwonsean <gownn12@naver.com>
2021-12-22 01:08:55 +0900

    feat: 카트 목록 컴포넌트 구현 완료

---
+++ client/src/components/cart/CartList.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Card, CardBody, CardFooter, CardHeader, Col, Row } from 'reactstrap'
+import styled from '../naver/ShoppingList.module.css'
+import CartTotalPrice from './CartTotalPrice'
+
+export default function CartList({ userId, cartId }) {
+  const [cartList, setCartList] = useState([])
+
+  useEffect(() => {
+    axios
+      .post('api/cart?type=list', {
+        user_id: userId,
+      })
+      .then((response) => {
+        const data = response.data.json
+        setCartList(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <h3>장바구니 목록</h3>
+        </CardHeader>
+        <CardBody
+          style={{
+            padding: 40,
+            height: 500,
+            overflow: 'scroll',
+          }}
+        >
+          {cartList.length > 0 && (
+            <Row className={styled.rowTitle}>
+              <Col xs='1'>상품이미지</Col>
+              <Col xs='6'>상품 명</Col>
+              <Col xs='2'>가격</Col>
+              <Col xs='1'>수량</Col>
+              <Col xs='2'>합산 가격</Col>
+            </Row>
+          )}
+          {cartList.length > 0 &&
+            cartList.map((item) => {
+              const htmlTitle = item.title
+              return (
+                <Row key={item.productId} className={styled.row}>
+                  <Col xs='1'>
+                    <img alt='item' src={item.image} width='100%' />
+                  </Col>
+                  <Col xs='6'>
+                    <h5
+                      className={styled.title}
+                      dangerouslySetInnerHTML={{ __html: htmlTitle }}
+                    ></h5>
+                  </Col>
+                  <Col xs='2'>
+                    <span>
+                      {item.l_price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원
+                    </span>
+                  </Col>
+                  <Col xs='1'>
+                    <span>{item.amount}</span>
+                  </Col>
+                  <Col xs='2'>
+                    <span>
+                      {item.price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원
+                    </span>
+                  </Col>
+                </Row>
+              )
+            })}
+        </CardBody>
+        <CardFooter>
+          <CartTotalPrice userId={userId} cartId={cartId} />
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
