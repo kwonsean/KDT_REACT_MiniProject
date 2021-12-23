de1ea466f97966bc9d865c605e0d9cf3fcaed823
kwonsean <gownn12@naver.com>
2021-12-24 01:33:24 +0900

    feat: 로그인 기능 구현
    - 수업을 통하여 로그인 기능 구현 완료

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,6 +13,9 @@ import History from './history/History'
 import Naver from './naver/Naver'
 import { useEffect, useState } from 'react'
 import getCartId from './modules/getCartId'
+import { useLocation } from 'react-router-dom'
+import cookie from 'react-cookies'
+import axios from 'axios'
 
 function App() {
   const [userId, setUserId] = useState('ksh96@naver.com')
@@ -21,7 +24,71 @@ function App() {
   useEffect(() => {
     const data = getCartId(userId)
     data.then((data) => setCartId(data))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const location = useLocation()
+
+  useEffect(() => {
+    if ('/register' !== location.pathname) {
+      fncCheckSession()
+    }
+    // console.log(location.pathname)
+  }, [location.pathname])
+
+  const fncCheckSession = async () => {
+    const tokenId = cookie.load('token_id')
+    const tokenName = cookie.load('token_name')
+    const userPwd = cookie.load('user_password')
+    // console.log(tokenId, tokenName, userPwd)
+    if (tokenName && tokenId) {
+      axios
+        .post('api/user?type=sessionCheck', {
+          token_id: tokenId,
+          token_name: tokenName,
+        })
+        .then((response) => {
+          // console.log(response.data)
+          // 유저 아이디 세팅
+          setUserId(response.data.decrypt_id.user_email)
+          // 쿠키의 password 검증
+          if (userPwd) {
+            axios
+              .post('/api/user?type=sessionSignin', {
+                user_email: response.data.decrypt_id.user_email,
+                user_password: userPwd,
+              })
+              .then((response) => {
+                if (!response.data[0].user_email) {
+                  // 로그인 상태 해제처리
+                  fncNotLogin()
+                }
+              })
+              .catch((error) => console.log(error))
+          } else {
+            // 로그인 상태 해제처리
+            fncNotLogin()
+          }
+        })
+    } else {
+      // 로그인 상태 해제처리
+      fncNotLogin()
+    }
+  }
+
+  // 검증실패 or token 정보 없을 경우 로그인 상태 해제 처리
+  const fncNotLogin = () => {
+    if (window.location.hash !== 'nocookie') {
+      fncRemoveCookie()
+    }
+    window.location.href = '/login/#nocookie'
+  }
+
+  const fncRemoveCookie = () => {
+    cookie.remove('token_id', { path: '/' }) // 전체에서 삭제 (path '/')
+    cookie.remove('token_name', { path: '/' })
+    cookie.remove('user_password', { path: '/' })
+  }
 
   return (
     <div className='App'>
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,22 +1,95 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {
-  Button,
-  Container,
-  Input,
-  InputGroup,
-  InputGroupText,
-} from 'reactstrap'
+import { Button, Input, InputGroup, InputGroupText } from 'reactstrap'
+import Swal from 'sweetalert2'
+import cookie from 'react-cookies'
 
 const Login = (props) => {
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+  const fncLogin = () => {
+    if (!(userEmail && userPassword)) {
+      sweetalert('이메일과 비밀번호를 확인해주세요', '', 'info')
+    }
+    axios
+      .post('/api/user?type=login', {
+        user_email: userEmail,
+        user_password: userPassword,
+      })
+      .then((response) => {
+        if (response.data[0] === undefined) {
+          sweetalert('아이디 또는 비밀번호가 일치하지 않습니다.', '', 'error')
+          return
+        }
+
+        let user_email = response.data[0].user_email
+        let user_name = response.data[0].user_name
+        let user_pwd = response.data[0].user_password
+
+        if (user_email) {
+          sweetalert('로그인 되었습니다.', '', 'info', '닫기')
+
+          // 로그인 후에 세션 유효기간 60분으로 설정하기
+          const expires = new Date()
+          expires.setMinutes(expires.getMinutes() + 60)
+          axios
+            .post('/api/user?type=webtoken', {
+              user_email,
+              user_name,
+            })
+            .then((response) => {
+              // console.log(response.data)
+              // console.log('token_id', response.data.token_id)
+              // console.log('token_name', response.data.token_name)
+
+              cookie.save('token_id', response.data.token_id, {
+                path: '/', // 모든 url에 적용
+                expires, // 위에서 선언한 세션 유효 기간
+              })
+              cookie.save('token_name', response.data.token_name, {
+                path: '/', // 모든 url에 적용
+                expires,
+              })
+              cookie.save('user_password', user_pwd, {
+                path: '/',
+                expires,
+              })
+            })
+            .catch((error) => {
+              sweetalert('작업중 오류가 발생했습니다.', error, 'error')
+            })
+          setTimeout(function () {
+            window.location.href = '/naverApi'
+          }, 1000)
+        }
+      })
+  }
+
+  const sweetalert = (title, showConfirmButton, icon) => {
+    Swal.fire({
+      position: 'bottom-end',
+      icon: icon,
+      title: title,
+      showConfirmButton: showConfirmButton,
+      timer: 1000,
+    })
+  }
+
   return (
-    <Container>
+    <div
+      style={{
+        width: '100%',
+        backgroundColor: 'royalBlue',
+        padding: '150px 200px',
+      }}
+    >
       <div
         style={{
           width: '60%',
           margin: '0 auto',
           padding: '30px 60px',
-          backgroundColor: 'royalBlue',
         }}
       >
         <h1 style={{ marginBottom: 20, color: 'white', textAlign: 'center' }}>
@@ -26,22 +99,30 @@ const Login = (props) => {
           <InputGroupText style={{ display: 'block', width: 50 }}>
             @
           </InputGroupText>
-          <Input placeholder='ID' />
+          <Input
+            placeholder='ID'
+            onChange={(e) => setUserEmail(e.target.value)}
+          />
         </InputGroup>
         <InputGroup>
           <InputGroupText style={{ display: 'block', width: 50 }}>
             PW
           </InputGroupText>
-          <Input placeholder='Password' />
+          <Input
+            placeholder='Password'
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
         </InputGroup>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Link to='/register' style={{ color: 'white', margin: '20px 0 0 0' }}>
             회원가입
           </Link>
-          <Button style={{ marginTop: 20 }}>로그인</Button>
+          <Button style={{ marginTop: 20 }} onClick={fncLogin}>
+            로그인
+          </Button>
         </div>
       </div>
-    </Container>
+    </div>
   )
 }
 
