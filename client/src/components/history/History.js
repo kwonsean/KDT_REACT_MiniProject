3b247c8672d8c6b25cf8b27bfac6526814655b2c
kwonsean <gownn12@naver.com>
2021-12-24 03:13:21 +0900

    feat: 구매내역 검색기능 구현을 위한 코드 수정

--- client/src/components/history/History.js
+++ client/src/components/history/History.js
@@ -1,11 +1,38 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'reactstrap'
 import Chart from './Chart'
 import ChartDetail from './ChartDetail'
 import HistoryList from './HistoryList'
+import HistorySearch from './HistorySearch'
+
+const year = String(new Date().getFullYear())
+const month = String(new Date().getMonth() + 1)
+const day = String(new Date().getDate())
+const today = year + month + day
 
 const History = ({ userId }) => {
   const [selectedCategory, setSelectedCategory] = useState('없음')
+  const [orderList, setOrderList] = useState([])
+
+  function getOrderList(startDate = '20211220', endDate = today) {
+    axios
+      .post('api/order?type=list', {
+        user_id: userId,
+        start_date: startDate,
+        end_date: endDate,
+      })
+      .then((response) => {
+        setOrderList(response.data.json)
+      })
+      .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getOrderList()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Container>
       <h2>사용자 구매내역</h2>
@@ -21,7 +48,8 @@ const History = ({ userId }) => {
         <Chart userId={userId} setSelectedCategory={setSelectedCategory} />
         <ChartDetail userId={userId} selectedCategory={selectedCategory} />
       </div>
-      <HistoryList userId={userId} />
+      <HistorySearch getOrderList={getOrderList} />
+      <HistoryList userId={userId} orderList={orderList} />
     </Container>
   )
 }
--- client/src/components/history/HistoryList.jsx
+++ client/src/components/history/HistoryList.jsx
@@ -1,53 +1,52 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Card, CardBody, CardFooter, CardHeader, Input } from 'reactstrap'
+import React from 'react'
+import { Card, CardBody, CardFooter, CardHeader } from 'reactstrap'
 import HistoryBody from './HistoryBody'
 
-const year = String(new Date().getFullYear())
-const month = String(new Date().getMonth() + 1)
-const day = String(new Date().getDate())
-
-export default function HistoryList({ userId }) {
-  const [orderList, setOrderList] = useState([])
-
-  useEffect(() => {
-    axios
-      .post('api/order?type=list', {
-        user_id: userId,
-        start_date: '20211220',
-        end_date: year + month + day,
-      })
-      .then((response) => {
-        setOrderList(response.data.json)
-      })
-      .catch((error) => console.log(error))
-  }, [])
-
+export default function HistoryList({ userId, orderList }) {
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {orderList.map((item) => (
-        <Card
-          style={{ margin: '0 2.5% 20px 2.5%', width: '45%' }}
-          key={item.order_id}
+      {orderList.length > 0 ? (
+        orderList.map((item) => (
+          <Card
+            style={{
+              margin: '0 2.5% 20px 2.5%',
+              width: '45%',
+              boxShadow: 'rgba(0, 0, 0, 0.04) 0px 3px 5px',
+            }}
+            key={item.order_id}
+          >
+            <CardHeader>
+              <h6 style={{ display: 'inline-block', marginRight: 10 }}>
+                주문 번호
+              </h6>{' '}
+              <span> {item.order_id}</span>
+            </CardHeader>
+            <CardBody style={{ maxHeight: 400, overflow: 'scroll' }}>
+              <HistoryBody item={item} userId={userId} />
+            </CardBody>
+            <CardFooter style={{ textAlign: 'right' }}>
+              전체 결제 금액
+              <b style={{ marginLeft: 5 }}>
+                {String(item.total_price).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+              </b>{' '}
+              원
+            </CardFooter>
+          </Card>
+        ))
+      ) : (
+        <div
+          style={{
+            padding: 30,
+            margin: '0 auto',
+
+            fontSize: 30,
+            fontWeight: 600,
+            textAlign: 'center',
+          }}
         >
-          <CardHeader>
-            <h6 style={{ display: 'inline-block', marginRight: 10 }}>
-              주문 번호
-            </h6>{' '}
-            <span> {item.order_id}</span>
-          </CardHeader>
-          <CardBody style={{ maxHeight: 400, overflow: 'scroll' }}>
-            <HistoryBody item={item} userId={userId} />
-          </CardBody>
-          <CardFooter style={{ textAlign: 'right' }}>
-            전체 결제 금액
-            <b style={{ marginLeft: 5 }}>
-              {String(item.total_price).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
-            </b>{' '}
-            원
-          </CardFooter>
-        </Card>
-      ))}
+          검색결과가 없습니다!
+        </div>
+      )}
     </div>
   )
 }
