77442b9774c5b2c64c8be12e0b02f7394e88c15a
kwonsean <gownn12@naver.com>
2021-12-23 19:26:04 +0900

    feat: 구매내역 페이지 완료
    - TODO 전체 요소가 한번에 컨트롤 되는 문제 해결해야함

--- client/src/components/history/History.js
+++ client/src/components/history/History.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'reactstrap'
+import Chart from './Chart'
+import ChartDetail from './ChartDetail'
 import HistoryList from './HistoryList'
 
-const History = ({ cartId, userId }) => {
+const History = ({ userId }) => {
+  const [selectedCategory, setSelectedCategory] = useState('없음')
   return (
     <Container>
       <h2>사용자 구매내역</h2>
+      <div
+        style={{
+          width: '95%',
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Chart userId={userId} setSelectedCategory={setSelectedCategory} />
+        <ChartDetail userId={userId} selectedCategory={selectedCategory} />
+      </div>
       <HistoryList userId={userId} />
     </Container>
   )
